5f224f9e6b024de5df8ed786f83b7d0fadb44fbc
RISHALI <rrishali@gmail.com>
2017-01-22 21:39:31 +0530

    Add files via upload
    index.html is the solution for question 1
    style.css is the solution for question 2
    app.js is the solution for question 3

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+    var x = document.getElementById("myPsw").value;
+	var y = document.getElementById("myusr").value;
+	document.write("username = "+y+"</br>");
+	document.write("passwordEntered = "+x+"</br>");
+	console.log(y);
+	console.log(x);
+}
